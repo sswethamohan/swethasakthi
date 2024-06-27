0a1c4604057a1f242313752ae554977e4e0a68af
sswethamohan <mohanswetha944@gmail.com>
2024-06-27 09:34:09 +0530

    protfolio

---
+++ src/data/user_info.js
@@ -0,0 +1,117 @@
+const info = {
+    // ============ MAIN DETAILS ============
+    main: {
+        name: "Sweths Mohan",
+        description: "I am a Front-End-Developer with a deep passion for coding and problem solving, and good understanding knowledge of web designing and database in mysql...",
+        role: "Front-End-Developer",
+        photo: "../mypick.jpg",
+        email: "mohanswetha944@gmail.com",
+    },
+
+    // ============ SOCIAL LINKS ============
+    socials: {
+        // twitter: "https://twitter.com/dsbalico",
+		github: "https://github.com/sswethamohan",
+		linkedin: "https://www.linkedin.com/in/swetha-mohan-63a339252/",
+		instagram: "https://www.instagram.com/swethamohan732001/?hl=en",
+		// facebook: "https://facebook.com/dsbalico",
+    },
+
+    // ============ PROJECTS ============
+    projects: [
+        {
+			title: "Diabetes Prediction using machine learning",
+			description: "Pseudocode to C is a web app that translates pseudocode into C code, aiding in the transition from high-level logic to a lower-level language.",
+            technologies: "python with Machine learning",
+			github: "https://github.com/dsbalico/pseudocode-to-c",
+            link: "https://pseudocode-to-c.netlify.app/"
+		},
+
+        {
+			title: "Covid-19 Vaccination information Portal",
+			description: "In this covid-19 Vaccination information Portal is a web-based platform designed to provide the public with accurate and upto-date information about covid-19 vaccines",
+			technologies: "html,css,boostrap",
+			github: "https://github.com/dsbalico/YouShare",
+            link: "https://github.com/dsbalico/YouShare"
+		},
+
+    ],
+
+    // ============ EDUCATION ============
+    education: [
+        {
+            school: "Thiruvalluvar University",
+            degree: "Master of Science in Computer Science-88.45%",
+            duration: "2021 - 2023",
+            image: "thiru.jpg"
+        },
+        {
+            school: "Thiruvalluvar University",
+            degree: "Bachelor of Science in Computer Science-83.64%",
+            duration: "2018 - 2021",
+            image: "thiru.jpg"
+        },
+        {
+            school: "HSC",
+            degree: "Government girl higher secondary school-63%",
+            duration: "2018",
+            image: "school.png"
+        },
+        {
+            school: "SSLC",
+            degree: "National higher secondary school-63%",
+            duration: "2016",
+            image: "school.png"
+        },
+
+    ],
+
+    // ============ EXPERIENCE ============
+    experience: [
+        {
+            position: "Web Developer intern",
+            company: "Corpwings IT Services & Consultance",
+            duration: "(3 Month)",
+            image: "spvttc.webp",
+            descriptions:[
+                "Developed My-Protfolio React app using react js with tailwind css "
+                // "Developed Python scripts to automate document score calculations.",
+                // "Developed a company website as part of an ICT-related work immersion program."
+            ]
+        },
+    ],
+
+    // ============ CERTIFICATES ============
+    certificates: [
+        {
+            title: "Besant Technology",
+            description:"PYTHON PROGRAMMING Certification course in ( FULL STACK PYTHON ) from Besant Technologies ,Rajajinagar ,Bangalore",
+             icon: "",
+            // link: "https://www.coursera.org/account/accomplishments/records/SF2PPLNVTH6R",
+        },
+        {
+            title: "Foundational Course in Cyber Security(SFC101)",
+            description: "Aligned to Competency Standards developed by IT-ITeS Sector Skilld Council NASSCOM in collabaration with industry and approved by Government",
+            icon: "",
+            // link: "https://www.credly.com/badges/fa00026a-4db5-4269-ab4f-340f5382ac03",
+        },
+        {
+            title: "Swayam Online Course",
+            description: "web Based Technologies and Multimedia Application with a conosolidation score of 62%",
+            icon: "",
+            // link: "https://www.credly.com/badges/fa00026a-4db5-4269-ab4f-340f5382ac03",
+        },
+        
+    ],
+
+    // ============ CONTACT ============
+    contact: {
+        title: "Let's Get in Touch: Ways to Connect with Me",
+        description: "Thank you for your interest in getting in touch with me. I welcome your feedback, questions, and suggestions. If you have a specific question or comment, please feel free to email me directly at mohanswetha944@gmail.com. I make an effort to respond to all messages within 24 hours, although it may take me longer during busy periods. Finally, if you prefer to connect on social media, you can connect with me using the social media links below.",
+    },
+
+    // ============ FOOTER ============
+    footer: "© 2024 Swetha Mohan. All Rights Reserved"
+}
+
+export default info
